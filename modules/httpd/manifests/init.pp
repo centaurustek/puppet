da3cc6c1dc6cf548d6a524781cefdfc69630b179
uday7g <root@slave.(none)>
2015-09-18 17:11:43 -0700

    deleted duplicate package resourcetype

--- modules/httpd/manifests/init.pp
+++ modules/httpd/manifests/init.pp
@@ -2,7 +2,7 @@ class httpd {
 file { “/etc/httpd/conf/httpd.conf”:
 ensure => present,
 require => Package[‘httpd’],
-notify => Service[‘httpd’],
+#notify => Service[‘httpd’],
 }
 service { “httpd”:
 ensure => running,
@@ -11,11 +11,4 @@ enable => true,
 package { “httpd”:
 ensure => present
 }
-file { “/etc/httpd/conf.d/vhost.conf”:
-ensure => present,
-source => “puppet:///httpd/vhost.conf”,
-owner => root,
-group => root,
 }
-}
-#this is test
