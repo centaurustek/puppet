50b825c95ea22c07354e3c2f0a2503dbeb8b4e73
uday7g <uday7g@users.noreply.github.com>
2016-03-28 19:43:15 -0500

    params.pp

---
+++ modules/params/params.pp
@@ -0,0 +1,9 @@
+class selectstate::params {
+$packagename = $hostname ? {
+               'RedHat' => 'tree',
+               'client' => 'telnet',
+                default => 'wget',
+               }
+
+}
+~   
